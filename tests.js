49b75d00fa66540f58bbd66bcefcf13fe0d9a3ad
Nail Gimaev <gimntut@list.ru>
2015-07-12 21:56:54 +0500

    Покрыл собственную библиотеку тестами. Довёл до работоспособности.

--- tests.js
+++ tests.js
@@ -1,15 +1,69 @@
 QUnit.test("joke-heaven wrapper tests", function(assert) {
     assert.ok(true, "");
-    databoom.connectTo('gimntut', 'elf-chat');
-    databoom.auth('user', 'user');
-    databoom.addToCollection('table1', '[{"a":"2"}]', success);
+    var db = connectTo('gimntut', 'elf-chat');
+    db.login('user', 'user');
+    db.save('table1', '[{"id":"test", "a":"2"}]').done(checkInsert).fail(fail);
 
-    function success(response) {
-        console.log(response);
-        assert.ok(1 == "1", "Passed!");
+    function checkInsert(data, textStatus, jqXHR) {
+        console.log(data);
+        assert.ok(true, "checkInsert passed!");
+        db.get({
+            'path': 'table1(test)'
+        }).done(checkGet).fail(fail);
+    };
+
+    function checkGet(data, textStatus, jqXHR) {
+        console.log(data);
+        var object = data.d.results[0];
+        assert.ok(object.a == "2", "checkGet passed!");
+        object.a = "3";
+        db.save('table1', JSON.stringify(object)).done(checkUpdate);
+    };
+
+    function checkUpdate(data, textStatus, jqXHR) {
+        console.log(data);
+        assert.ok(true, "checkUpdate passed!");
+        db.get({
+            'path': 'table1(test)'
+        }).done(checkGet2).fail(fail);
     }
+
+    function checkGet2(data, textStatus, jqXHR) {
+        console.log(data);
+        var object = data.d.results[0];
+        assert.ok(object.a == "3", "checkGet2 passed!");
+        db.delete('table1(test)').done(checkDelete).fail(fail);
+    }
+
+    function checkDelete(data, textStatus, jqXHR) {
+        console.log(data);
+        assert.ok(true, "checkDelete passed!");
+        db.get({
+            'path': 'table1(test)'
+        }).done(checkDeleteEffect).fail(fail);
+    }
+
+    function checkDeleteEffect(data, textStatus, jqXHR) {
+        console.log(data);
+        assert.ok(data.d.results.length === 0, "checkDeleteEffect passed!");
+    }
+
+    function fail(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR, textStatus, errorThrown);
+        assert.push(true, true, "", textStatus);
+    };
 });
 
-QUnit.test("joke-heaven app tests", function(assert) {
-    assert.ok(1 == 2, "Passed!");
-});
+// QUnit.test("joke-heaven databoom.js tests", function(assert) {
+//     assert.ok(true, "");
+//     var db = databoom('http://gimntut.databoom.space', 'elf-chat');
+//     db.login('user', 'user');
+//     db.save('table1', [{
+//         "a": "30"
+//     }]).done(success);
+
+//     function success(data, textStatus, jqXHR) {
+//         console.log(data);
+//         assert.ok(1 == "1", "passed!");
+//     }
+// });
--- wrapper.js
+++ wrapper.js
@@ -1,31 +1,41 @@
-var databoom = {
-    userName: "",
-    dbName: "",
-    connectTo: function(userName, dbName) {
-        this.userName = userName;
-        this.dbName = dbName;
-    },
-    auth: function(login, password) {
-        $.ajaxSetup({
-            headers: {
-                'Authorization': "Basic " + btoa(login + ":" + password)
-            }
-        });
-    },
-    addToCollection: function(collection, jsonString, success) {
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://" + this.userName + ".databoom.space/api1/" + this.dbName + "/collections/" + collection,
-            "method": "POST",
-            "headers": {
-                "content-type": "application/json"
-            },
-            "data": jsonString
-        };
+function connectTo(userName, dbName) {
+    var dbUrl = "https://" + userName + ".databoom.space/api1/" + dbName;
+    var collectionPath = dbUrl + '/collections/';
+    return {
+        userName: userName,
+        dbName: dbName,
+        dbUrl: dbUrl,
+        collectionPath: collectionPath,
+        settings: function(method, path, data) {
+            return {
+                'async': true,
+                'crossDomain': true,
+                'url': this.collectionPath + path,
+                'method': method,
+                'headers': {
+                    'content-type': 'application/json'
+                },
+                'data': data
+            };
+        },
+        login: function(login, password) {
+            $.ajaxSetup({
+                headers: {
+                    'Authorization': "Basic " + btoa(login + ":" + password)
+                }
+            });
+        },
+        save: function(collection, jsonString, success) {
+            return $.ajax(this.settings('POST', collection, jsonString));
+        },
+        get: function(query) {
+            var s = this.settings('GET', query.path);
+            s.dataType = 'json';
+            return $.ajax(s);
+        },
+        delete: function(path) {
+            return $.ajax(this.settings('DELETE', path));
+        }
 
-        $.ajax(settings).done(success);
-
-    }
-
-};
+    };
+}
